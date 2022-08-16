50b0e467b73226296a051c9ac183b8314bb8c97a
HansBukerG <111304372+HansBukerG@users.noreply.github.com>
2022-08-16 00:07:09 -0400

    Delete Procedimientos Almacenados directory

--- Procedimientos Almacenados/Archivos sin uso/bitacora/bitacora_control_turnos.sql
+++
@@ -1,9 +0,0 @@
-create table dbo.BITACORA_CONTROL_TURNOS 
-(
-   OFRE_CODIGO          integer                        not null,
-   CCO_CODIGO           decimal                        not null,
-   REP_CODIGO           decimal                        not null,
-   EDU_CODIGO           integer                        not null,
-   BI_FECHA_REGISTRO    datetime                       null,
-   constraint PK_BITACORA_CONTROL_TURNOS primary key clustered (OFRE_CODIGO, CCO_CODIGO, REP_CODIGO, EDU_CODIGO)
-);
--- Procedimientos Almacenados/parametros_atencion_publico/parametros_atencion_publico.sql
+++
@@ -1,5 +0,0 @@
-create table DOCUMENTOS_UBB.DBO.PARAMETROS_ATENCION_PUBLICO 
-(
-   MODA_UNICA           integer                        null,
-   MODA_MODULAR         integer                        null
-);
